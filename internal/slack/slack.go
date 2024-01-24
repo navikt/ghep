35a40e9ed81de32642bcb362e791db9a0c5e0801
Kyrre Havik <kyrremann@gmail.com>
2024-01-24 08:42:25 +0100

    feat: Slack returnerer 200 ved feil
    Så man må parse det man får tilbake.

--- internal/slack/slack.go
+++ internal/slack/slack.go
@@ -3,7 +3,10 @@ package slack
 import (
 	"bytes"
 	"embed"
+	"encoding/json"
 	"fmt"
+	"io"
+	"log/slog"
 	"net/http"
 	"text/template"
 	"time"
@@ -40,23 +43,45 @@ func New(token string) (Client, error) {
 }
 
 func (c Client) PostMessage(payload []byte) error {
-	body := bytes.NewReader(payload)
-
-	req, err := http.NewRequest("POST", "https://slack.com/api/chat.postMessage", body)
+	req, err := http.NewRequest("POST", "https://slack.com/api/chat.postMessage", bytes.NewReader(payload))
 	if err != nil {
 		return err
 	}
 
-	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+c.token)
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return err
 	}
 
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	type slackResponse struct {
+		Ok    bool   `json:"ok"`
+		Error string `json:"error"`
+		Warn  string `json:"warning"`
+	}
+
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("error posting message to Slack(%v): %v", resp.Status, resp.Body)
+		return fmt.Errorf("error posting message to Slack(%v): %v", resp.Status, body)
+	}
+
+	var slackResp slackResponse
+	if err := json.Unmarshal([]byte(body), &slackResp); err != nil {
+		return err
+	}
+
+	if !slackResp.Ok {
+		return fmt.Errorf("error posting message to Slack: %v", slackResp.Error)
+	}
+
+	if slackResp.Warn != "" {
+		slog.Info("warning posting message to Slack", "warn", slackResp.Warn)
 	}
 
 	return nil
