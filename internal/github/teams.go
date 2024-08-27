61004afb2c8071d5182b49d711c1ee15b069208e
Kyrre Havik <kyrremann@gmail.com>
2024-08-27 15:40:15 +0200

    fix: glemte å sette objektet tilbake i lista

--- internal/github/teams.go
+++ internal/github/teams.go
@@ -160,7 +160,7 @@ func FetchTeams(githubAPI, appInstallationID, appID, appPrivateKey, githubOrg, t
 
 	reposBlocklist := strings.Split(reposBlocklistString, ",")
 
-	for _, team := range teams {
+	for i, team := range teams {
 		tmplData["team"] = team.Name
 
 		var url strings.Builder
@@ -174,6 +174,7 @@ func FetchTeams(githubAPI, appInstallationID, appID, appPrivateKey, githubOrg, t
 		}
 
 		team.Repositories = repos
+		teams[i] = team
 	}
 
 	return teams, nil
