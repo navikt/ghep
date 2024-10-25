ca9b054eb904bfb025494c9b1d7e60fc8c44522a
Kyrre Havik <kyrremann@gmail.com>
2024-10-25 13:01:14 +0200

    feat: fjerner tidligere reactions

--- internal/slack/reactions.go
+++ internal/slack/reactions.go
@@ -31,7 +31,40 @@ func (c Client) PostWorkflowReaction(log *slog.Logger, team github.Team, event g
 		return fmt.Errorf("posting reaction to workflow event: %v", err)
 	}
 
+	return c.RemoveOtherReactions(log, team.SlackChannels.Commits, timestamp, reaction)
+}
+
+func (c Client) RemoveOtherReactions(log *slog.Logger, channel, timestamp, reaction string) error {
+	log.Info("Removing other reactions", "channel", channel, "timestamp", timestamp, "current_reaction", reaction)
+	reactions, err := c.GetReactions(channel, timestamp)
+	if err != nil {
+		return fmt.Errorf("getting reactions: %v", err)
+	}
+
+	for _, r := range reactions {
+		if r != reaction {
+			log.Info("Removing reaction", "reaction", r)
+			if err := c.reactionRequest("remove", r, channel, timestamp); err != nil {
+				return fmt.Errorf("removing reaction: %v", err)
+			}
+		}
+	}
+
 	return nil
+}
+
+func (c Client) GetReactions(channel, timestamp string) ([]string, error) {
+	payload := map[string]string{
+		"channel":   channel,
+		"timestamp": timestamp,
+	}
+
+	marshalled, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	_, err = c.request("reactions.get", marshalled)
+	return nil, fmt.Errorf("reactions.get: %v", err)
 }
 
 func (c Client) reactionRequest(method, channel, reaction, timestamp string) error {
