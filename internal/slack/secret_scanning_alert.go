ac5f8c66443d6af2ace2c9ad0cdfc9558ce19064
Kyrre Havik <kyrre.havik@nav.no>
2025-06-24 15:26:09 +0200

    feat: penere Slackmelding

--- internal/slack/secret_scanning_alert.go
+++ internal/slack/secret_scanning_alert.go
@@ -12,11 +12,11 @@ func CreateSecretScanningAlertMessage(channel string, event github.Event) *Messa
 	case "created":
 		text = fmt.Sprintf("A secret scanning alert was just %s for the repository %s.\nThe secret was of type `%s`.\nRead more: %s", event.Action, event.Repository.ToSlack(), *event.Alert.SecretType, event.Alert.URL)
 	case "resolved":
-		text = fmt.Sprintf("A secret scanning alert was just %s for the repository %s. It was resolved as `%s` (%s) by %s.\nRead more: %s", event.Action, event.Repository.ToSlack(), event.Alert.Resolution, event.Alert.ResolutionComment, event.Alert.ResolvedBy.ToSlack(), event.Alert.URL)
+		text = fmt.Sprintf("A secret scanning alert was just %s for the repository %s. It was resolved as `%s` by %s.\n> %s", event.Action, event.Repository.ToSlack(), event.Alert.Resolution, event.Alert.ResolvedBy.ToSlack(), event.Alert.ResolutionComment)
 	}
 
 	var attachments []Attachment
-	if event.Alert.PubliclyLeaked {
+	if event.Alert.PubliclyLeaked && event.Action == "created" {
 		attachment := Attachment{
 			Text:  "The secret was publicly leaked!",
 			Color: ColorCritical,
