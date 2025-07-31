9ba5f69d4c51539edf36733752250df5c9fab005
Kyrre Havik <kyrre.havik@nav.no>
2025-07-31 16:45:19 +0200

    fix: her var det brukt feil timestamp felt

--- internal/slack/dependabot_alert.go
+++ internal/slack/dependabot_alert.go
@@ -18,9 +18,9 @@ func CreateDependabotAlertMessage(channel string, event github.Event, timestamp 
 	}
 
 	return &Message{
-		Channel:     channel,
-		Text:        fmt.Sprintf("A Dependabot alert was just %s for the repository %s.\nRead more: %s", event.Action, event.Repository.ToSlack(), event.Alert.URL),
-		Timestamp:   timestamp,
-		Attachments: attachments,
+		Channel:         channel,
+		Text:            fmt.Sprintf("A Dependabot alert was just %s for the repository %s.\nRead more: %s", event.Action, event.Repository.ToSlack(), event.Alert.URL),
+		ThreadTimestamp: timestamp,
+		Attachments:     attachments,
 	}
 }
